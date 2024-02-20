6ab26019b35c5317b49215ebbbb576074334d305
Kristers Pulle <pulle265@gmail.com>
2024-02-20 19:12:46 +0200

    feat(homepage, registration page) implemented registration and home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 'use client';
 
+import styles from './page.module.css';
 import Link from 'next/link';
+import Image from 'next/image';
 import { Button } from './components/Button/Button';
 import { redirect, useRouter } from 'next/navigation';
 import { useSession } from 'next-auth/react';
@@ -13,16 +15,63 @@ const Home = () => {
   }
 
   return (
-    <>
-      <div>
-        <Button
-          type="button"
-          text="Register"
-          onClick={() => router.push('/register')}
-        />
-        <Link href={'/api/auth/signin'}>Log in</Link>
+    <main className={styles.mainWrapper}>
+      <div className={styles.header}>
+        <div className={styles.logo}>
+          <Image
+            priority={true}
+            width={50}
+            height={50}
+            src="/logo.png"
+            alt={'icon'}
+          />
+          Wallet
+        </div>
+        <div className={styles.authButtons}>
+          <Button
+            type="button"
+            text="Register"
+            onClick={() => router.push('/register')}
+          />
+          <Link href={'/api/auth/signin'} className={styles.signIn}>
+            Log in
+          </Link>
+        </div>
       </div>
-    </>
+      <section className={styles.hero}>
+        <div className={styles.wrapper}>
+          <div className={styles.titleWrapper}>
+            <h1 className={styles.heading}>Virtual Wallet</h1>
+            <p className={styles.paragraph}>
+              Virtual wallet created to follow up on your daily spendings and
+              budget planning
+            </p>
+            <Image
+              priority={true}
+              width={600}
+              height={508}
+              src="/graph.png"
+              alt={'graph'}
+            />
+          </div>
+          <div className={styles.vectorWrapper}>
+            <Image
+              priority={true}
+              width={684}
+              height={456}
+              src="/laptop.png"
+              alt={'laptop'}
+            />
+            <ul className={styles.list}>
+              <li>- Create multiple wallets</li>
+              <li>- Manage transactions of all the wallets</li>
+              <li>- See all the transactions in one place</li>
+              <li>- Plan your budget for maximized savings</li>
+            </ul>
+          </div>
+        </div>
+      </section>
+    </main>
   );
 };
 
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import styles from './page.module.css';
 import { useState } from 'react';
 import { Button } from '../components/Button/Button';
 import { Input } from '../components/Input/Input';
@@ -38,47 +39,54 @@ const Register = () => {
     }
   };
   return (
-    <form
-      onSubmit={(e) => {
-        e.preventDefault();
-        handleSubmit();
-      }}
-    >
-      <Input
-        required={true}
-        placeholder="Username"
-        id="username"
-        label="Enter your username"
-        type="text"
-        value={formValues.username}
-        onChange={(e) => {
-          setFormValues({ ...formValues, username: e.target.value });
+    <div className={styles.wrapper}>
+      <h1>Create a new account</h1>
+      <form
+        className={styles.form}
+        onSubmit={(e) => {
+          e.preventDefault();
+          handleSubmit();
         }}
-      />
-      <Input
-        required={true}
-        placeholder="Password"
-        id="password"
-        label="Enter your password"
-        type="password"
-        value={formValues.password}
-        onChange={(e) => {
-          setFormValues({ ...formValues, password: e.target.value });
-        }}
-      />
-      <Input
-        required={true}
-        placeholder="Password"
-        id="repeatedPassword"
-        label="Repeat your password"
-        type="password"
-        value={formValues.repeatedPassword}
-        onChange={(e) => {
-          setFormValues({ ...formValues, repeatedPassword: e.target.value });
-        }}
-      />
-      <Button text="Register" type="submit" />
-    </form>
+      >
+        <Input
+          required={true}
+          placeholder="Username"
+          id="username"
+          label="Enter your username"
+          type="text"
+          value={formValues.username}
+          onChange={(e) => {
+            setFormValues({ ...formValues, username: e.target.value });
+          }}
+        />
+        <Input
+          required={true}
+          placeholder="Password"
+          id="password"
+          label="Enter your password"
+          type="password"
+          value={formValues.password}
+          onChange={(e) => {
+            setFormValues({ ...formValues, password: e.target.value });
+          }}
+        />
+        <Input
+          required={true}
+          placeholder="Password"
+          id="repeatedPassword"
+          label="Repeat your password"
+          type="password"
+          value={formValues.repeatedPassword}
+          onChange={(e) => {
+            setFormValues({ ...formValues, repeatedPassword: e.target.value });
+          }}
+        />
+        <div className={styles.buttonWrapper}>
+
+        <Button text="Register" type="submit" />
+        </div>
+      </form>
+    </div>
   );
 };
 
