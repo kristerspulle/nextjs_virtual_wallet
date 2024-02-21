297eb03d97533f6826aea6b47561867c7a6a43fa
Kristers Pulle <pulle265@gmail.com>
2024-02-21 18:08:46 +0200

    fix(next-auth) route update

--- app/api/auth/[...nextauth]/route.tsx
+++ app/api/auth/[...nextauth]/route.tsx
@@ -1,7 +1,6 @@
-import { NextApiHandler } from 'next';
 import NextAuth from 'next-auth';
 import { authOptions } from '@/libs/services/authOptions';
 
-const handler: NextApiHandler = NextAuth(authOptions);
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
