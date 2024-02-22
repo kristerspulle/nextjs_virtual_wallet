0e872111ad10abd7d95580f505fc08e8fc60485b
Kristers Pulle <pulle265@gmail.com>
2024-02-22 21:21:11 +0200

    middleware update

--- middleware.tsx
+++ middleware.tsx
@@ -1,3 +1,9 @@
 export { default } from "next-auth/middleware"
 
 export const config = { matcher: "/protected/(.*)"}
+
+export const withAuth = ({
+  pages: {
+    signIn: 'api/auth/login', 
+  }
+});
