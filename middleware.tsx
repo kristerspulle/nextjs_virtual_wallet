224fcd60027c8437e373d8c79e5c60aadfd20456
Kristers Pulle <pulle265@gmail.com>
2024-02-26 15:06:32 +0200

    updated file extensions

--- middleware.tsx
+++ middleware.tsx
@@ -1,3 +1,4 @@
+import { useSession } from 'next-auth/react';
 import { NextRequest, NextResponse } from 'next/server';
 import { NextApiRequest } from 'next/types';
 export { default } from "next-auth/middleware"
@@ -6,8 +7,6 @@ export const config = { matcher: "/protected/(.*)"}
 
 // Using Next.js 
 export function authenticationMiddleware(request: NextApiRequest) {
-  console.log(request.headers.cookie);
-  
   if (!request.headers.cookie?.includes('authenticated=true')) {
 
     return NextResponse.redirect('/api/auth/signin');
