dea717388ef237602d23921f172c741ea0885244
Kristers Pulle <pulle265@gmail.com>
2024-02-26 13:24:12 +0200

    fix

--- middleware.tsx
+++ middleware.tsx
@@ -1,3 +1,14 @@
-// export { default } from "next-auth/middleware"
+import { NextRequest, NextResponse } from 'next/server';
+import { NextApiRequest } from 'next/types';
+export { default } from "next-auth/middleware"
 
-// export const config = { matcher: "/protected/(.*)"}
+export const config = { matcher: "/protected/*"}
+
+// Using Next.js 
+export function authenticationMiddleware(request: NextApiRequest) {
+  if (!request.headers.cookies?.includes('authenticated=true')) {
+
+    return NextResponse.redirect('/api/auth/signin');
+  }
+  return NextResponse.next();
+}
