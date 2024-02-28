2d2f1a07decea3d5cdfcf95a44ea93a92496545e
Kristers Pulle <pulle265@gmail.com>
2024-02-28 15:06:06 +0200

    update

--- app/protected/wallets/page.tsx
+++ app/protected/wallets/page.tsx
@@ -1,11 +1,15 @@
 import { headers } from 'next/headers';
 import WalletPage from './WalletPage';
+import { getServerSession } from 'next-auth';
+import authOptions from '@/libs/services/authOptions';
 
 const getWallets = async () => {
   const response = await fetch(`${process.env.NEXTAUTH_URL}/api/wallets`, {
     cache: 'no-store',
-    headers: headers()
+    headers: new Headers(headers())
   });
+  console.log(Object.fromEntries(headers()));
+  
   if(!response.ok) {
     console.log(response)
   } else {
@@ -14,8 +18,11 @@ const getWallets = async () => {
 }
 
 const Wallets: React.FC = async () => {
-
+  // const session = await getServerSession(authOptions)
+  // const userId = session?.user?.id
+  // console.log(session, userId);
   const wallets = await getWallets()
+  
   return(
     <WalletPage wallets={wallets}/>
   )
