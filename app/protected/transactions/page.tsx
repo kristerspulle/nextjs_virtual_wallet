f2ba2d9cf688ee256d96dba0e634437a02681d0a
Kristers Pulle <pulle265@gmail.com>
2024-03-15 21:23:07 +0200

    update

--- app/protected/transactions/page.tsx
+++ app/protected/transactions/page.tsx
@@ -1,22 +1,22 @@
 import { headers } from 'next/headers';
 import TransactionsPage from './TransactionsPage';
 
-// const getTransactions = async () => {
-//   const response = await fetch(`${process.env.NEXTAUTH_URL}/api/transactions`, {
-//     cache: 'no-store',
-//     headers: new Headers(headers())
-//   })
+const getTransactions = async () => {
+  const response = await fetch(`${process.env.NEXTAUTH_URL}/api/transactions`, {
+    cache: 'no-store',
+    headers: new Headers(headers())
+  })
   
-//   if(!response.ok) {
-//     console.log(response)
-//   } else {
-//     return response.json()
-//   }
-// }
+  if(!response.ok) {
+    console.log(response)
+  } else {
+    return response.json()
+  }
+}
 
 const Transactions = async () => {
-  // const transactions = await getTransactions()
-  const transactions: Transaction[] = []
+  const transactions = await getTransactions()
+  console.log(transactions)
   return(
     <TransactionsPage transactions={transactions}/>
   )
