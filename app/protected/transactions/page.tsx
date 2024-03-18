fff4de453a5027a6535f5355f5b5153272956ecd
Kristers Pulle <pulle265@gmail.com>
2024-03-18 19:12:06 +0200

    update

--- app/protected/transactions/page.tsx
+++ app/protected/transactions/page.tsx
@@ -1,30 +1,45 @@
 import { headers } from 'next/headers';
 import TransactionsPage from './TransactionsPage';
 
+// const getTransactions = async () => {
+//   try {
 
-const Transactions: React.FC = async () => {
-  const getTransactions = async () => {
-    try {
-  
-      const response = await fetch(`${process.env.NEXTAUTH_URL}/api/transactions`, {
-        cache: 'no-store',
-        headers: {
-          'Content-Type': 'application/json', // Adjusted Content-Type header
-        }
-      })
-      
-      if(!response.ok) {
-        console.log(response)
-      } else {
-        return response.json()
-      }
-    } catch (error) {
-        throw error
+//     const response = await fetch(`${process.env.NEXTAUTH_URL}/api/transactions`, {
+//       cache: 'no-store',
+//       headers: new Headers(headers())
+//     })
+//     if(!response.ok) {
+//       console.log(response)
+//     } else {
+//       console.log(response)
+//       return response.json()
+//     }
+//   } catch (error) {
+//       throw error
+//   }
+// }
+const getTransactions = async () => {
+  try {
+    const response = await fetch(`${process.env.NEXTAUTH_URL}/api/transactions`, {
+      cache: 'no-store',
+      headers: new Headers(headers())
+    });
+
+    if (!response.ok) {
+      console.error('Failed to fetch transactions:', response.statusText);
+      return null;
     }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching transactions:', error);
+    return null;
   }
-
+};
+const Transactions: React.FC = async () => {
   const transactions = await getTransactions()
-
+  console.log(transactions)
   return(
     <TransactionsPage transactions={transactions}/>
   )
