e217bf8f71bc0ba9a7a8d500e49188e43dd3df5e
Kristers Pulle <pulle265@gmail.com>
2024-03-18 18:09:01 +0200

    update

--- app/protected/transactions/page.tsx
+++ app/protected/transactions/page.tsx
@@ -2,7 +2,7 @@ import { headers } from 'next/headers';
 import TransactionsPage from './TransactionsPage';
 
 const getTransactions = async () => {
-  const response = await fetch(`nextjs-virtual-wallet.vercel.app/api/transactions`, {
+  const response = await fetch(`${process.env.NEXTAUTH_URL}/api/transactions`, {
     cache: 'no-store',
     headers: new Headers(headers())
   })
