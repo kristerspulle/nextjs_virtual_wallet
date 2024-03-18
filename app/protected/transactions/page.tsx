d81a895d27ccde6beb4df812994d4eeca76b207a
Kristers Pulle <pulle265@gmail.com>
2024-03-18 18:29:07 +0200

    update

--- app/protected/transactions/page.tsx
+++ app/protected/transactions/page.tsx
@@ -1,26 +1,30 @@
 import { headers } from 'next/headers';
 import TransactionsPage from './TransactionsPage';
 
-const getTransactions = async () => {
-  try {
-
-    const response = await fetch(`${process.env.NEXTAUTH_URL}/api/transactions`, {
-      cache: 'no-store',
-      headers: new Headers(headers())
-    })
-    
-    if(!response.ok) {
-      console.log(response)
-    } else {
-      return response.json()
-    }
-  } catch (error) {
-      throw error
-  }
-}
 
 const Transactions: React.FC = async () => {
+  const getTransactions = async () => {
+    try {
+  
+      const response = await fetch(`${process.env.NEXTAUTH_URL}/api/transactions`, {
+        cache: 'no-store',
+        headers: {
+          'Content-Type': 'application/json', // Adjusted Content-Type header
+        }
+      })
+      
+      if(!response.ok) {
+        console.log(response)
+      } else {
+        return response.json()
+      }
+    } catch (error) {
+        throw error
+    }
+  }
+
   const transactions = await getTransactions()
+
   return(
     <TransactionsPage transactions={transactions}/>
   )
