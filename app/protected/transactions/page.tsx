5ab4a38cb5a8f0135f0a1eb868e9af234659db21
Kristers Pulle <pulle265@gmail.com>
2024-03-19 21:09:18 +0200

    update

--- app/protected/transactions/page.tsx
+++ app/protected/transactions/page.tsx
@@ -1,4 +1,4 @@
-import { headers } from 'next/headers';
+import { headers, cookies } from 'next/headers';
 import TransactionsPage from './TransactionsPage';
 
 // const getTransactions = async () => {
@@ -22,8 +22,9 @@ export const dynamic = 'force-dynamic';
 const getTransactions = async () => {
   try {
     const response = await fetch(`${process.env.NEXTAUTH_URL}/api/transactions`, {
-      cache: 'no-store',
-      headers: new Headers(headers())
+      cache: 'force-cache',
+      headers: new Headers(headers()),
+ 
     });
 
     if (!response.ok) {
